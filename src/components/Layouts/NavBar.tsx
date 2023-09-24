3d39110bd52dd493d0391aae47111a9f12ae34aa
zuraLagiNgoding <jibau19@gmail.com>
2023-09-24 08:42:33 +0700

    fix navbar ngaco

--- src/components/Layouts/NavBar.tsx
+++ src/components/Layouts/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
 
   return (
     <nav className="flex items-center bg-white justify-between px-64 py-6 w-full fixed z-50 shadow-md">
-      <a href="./" className="font-semibold text-3xl text-primary select-none">Wisata Padang</a>
+      <a href="/" className="font-semibold text-3xl text-primary select-none">Wisata Padang</a>
       <div className="flex items-center text-secondary font-semibold text-lg gap-4">
         {router.asPath.includes("/order") ?
         null
